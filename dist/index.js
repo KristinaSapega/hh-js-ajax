d276c37b2ebbba8d718f85e61de2e54d3812066c
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2024-01-19 17:38:10 +0300

    made changes

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const getData = (url) => __awaiter(void 0, void 0, void 0, function* () {
     const resp = yield fetch(url);
+    if (resp.status >= 400) {
+        throw new Error('Произошла ошибка при выполнении запроса. Статус ответа: ' + resp.status);
+    }
     const json = yield resp.json();
     return json;
 });
@@ -25,59 +28,82 @@ document.addEventListener("DOMContentLoaded", () => {
     const suggestionsList = document.getElementById("suggestions");
     const fullInfo = document.querySelector('.content-block');
     const searchHistoryList = document.getElementById("searchHistory");
+    const getSearchHistory = () => {
+        try {
+            return JSON.parse(localStorage.getItem("searchHistory") || "[]");
+        }
+        catch (error) {
+            return [];
+        }
+    };
+    let searchHistory = getSearchHistory();
     //загрузить историю поиска из localStorage
-    const searchHistory = JSON.parse(localStorage.getItem("searchHistory") || "[]");
-    searchHistory.slice(-3).forEach((query) => {
+    const createHistiryItem = (query) => {
         const item = document.createElement('div');
         item.innerText = query;
         searchHistoryList.appendChild(item);
+    };
+    const updateSearchHistory = () => {
+        searchHistoryList.innerHTML = '';
+        searchHistory.slice(-3).forEach((query) => {
+            const item = document.createElement('div');
+            item.innerText = query;
+            searchHistoryList.appendChild(item);
+        });
+    };
+    searchHistory.slice(-3).forEach(createHistiryItem);
+    window.addEventListener('storage', (event) => {
+        if (event.key === 'searchHistory') {
+            searchHistory = getSearchHistory();
+            updateSearchHistory();
+        }
     });
     searchInput.addEventListener('input', debounce(() => __awaiter(void 0, void 0, void 0, function* () {
         suggestionsList.innerHTML = '';
         if (searchInput.value === "")
             return;
-        const query = encodeURIComponent(searchInput.value); //кодируется     значение из поля поиска для безопасного включения его в URL.
-        const { meals } = yield getData(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
-        if (!meals)
-            return;
-        const inputText = searchInput.value.toLowerCase();
-        const results = searchHistory
-            .filter((q) => q.toLowerCase().includes(inputText))
-            .map((q) => ({
-            name: q,
-            fromHistory: true,
-            data: meals.find(m => m.strMeal === q)
-        }))
-            .slice(0, 5);
-        results.push(...meals
-            .filter(m => !results.find((item) => item.name === m.strMeal))
-            .slice(0, 10 - results.length)
-            .map((m) => ({
-            name: m.strMeal,
-            fromHistory: false,
-            data: m
-        })));
-        results.forEach((item) => {
-            const elem = document.createElement('div');
-            elem.innerText = item.name;
-            suggestionsList.appendChild(elem);
-            elem.addEventListener('click', () => __awaiter(void 0, void 0, void 0, function* () {
-                if (!item.fromHistory) {
-                    searchHistory.push(item.name); //добавляется выбранный элемент в историю поиска, сохраняется обновленная история в локальное хранилище
-                    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-                }
-                fullInfo.innerHTML = `
-                    <div><img class="img" src="${item.data.strMealThumb}" alt></div>
-                    <div>${item.name}</div>
-                `;
-                suggestionsList.innerHTML = '';
-                searchHistoryList.innerHTML = '';
-                searchHistory.slice(-3).forEach((query) => {
-                    const item = document.createElement('div');
-                    item.innerText = query;
-                    searchHistoryList.appendChild(item);
-                });
-            }));
-        });
+        const query = encodeURIComponent(searchInput.value);
+        try {
+            const { meals } = yield getData(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+            if (!meals)
+                return;
+            const inputText = searchInput.value.toLowerCase();
+            const results = searchHistory
+                .filter((q) => q.toLowerCase().includes(inputText))
+                .map((q) => ({
+                name: q,
+                fromHistory: true,
+                data: meals.find(m => m.strMeal === q)
+            }))
+                .slice(0, 5);
+            results.push(...meals
+                .filter(m => !results.find((item) => item.name === m.strMeal))
+                .slice(0, 10 - results.length)
+                .map((m) => ({
+                name: m.strMeal,
+                fromHistory: false,
+                data: m
+            })));
+            results.forEach((item) => {
+                const elem = document.createElement('div');
+                elem.innerText = item.name;
+                suggestionsList.appendChild(elem);
+                elem.addEventListener('click', () => __awaiter(void 0, void 0, void 0, function* () {
+                    if (!item.fromHistory) {
+                        searchHistory.push(item.name);
+                        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+                        updateSearchHistory();
+                    }
+                    fullInfo.innerHTML = `
+                        <div><img class="img" src="${item.data.strMealThumb}" alt></div>
+                        <div>${item.name}</div>
+                    `;
+                    suggestionsList.innerHTML = '';
+                }));
+            });
+        }
+        catch (e) {
+            alert('Произошла ошибка при взаимодействии с сервером');
+        }
     }), 500));
 });
--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 const getData = async (url: string) => {
     const resp = await fetch(url)
+    if (resp.status >= 400) {
+        throw new Error('Произошла ошибка при выполнении запроса. Статус ответа: ' + resp.status)
+    }
     const json = await resp.json()
     return json
 }
@@ -12,71 +15,99 @@ const debounce = (func: Function, delay: number) => {
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (): void => {
     const searchInput = document.getElementById("searchInput") as HTMLInputElement;
     const suggestionsList = document.getElementById("suggestions") as HTMLUListElement;
     const fullInfo = document.querySelector('.content-block') as HTMLDivElement;
     const searchHistoryList = document.getElementById("searchHistory") as HTMLUListElement;
+
+    const getSearchHistory = () => {
+        try {
+            return JSON.parse(localStorage.getItem("searchHistory") || "[]")
+        }
+        catch (error) {
+            return []
+        }
+    }
+
+    let searchHistory: string[] = getSearchHistory()
     
     //загрузить историю поиска из localStorage
-    const searchHistory = JSON.parse(localStorage.getItem("searchHistory") || "[]")
-    searchHistory.slice(-3).forEach((query: string) => {
-        const item = document.createElement('div')
-        item.innerText = query
-        searchHistoryList.appendChild(item)
-    })
+    const createHistiryItem = (query: string) => {
+        const item = document.createElement('div');
+        item.innerText = query;
+        searchHistoryList.appendChild(item);
+    };
 
-    searchInput.addEventListener('input', debounce(async () => {
+    const updateSearchHistory = () => {
+        searchHistoryList.innerHTML = ''
+        searchHistory.slice(-3).forEach((query: string) => {
+            const item = document.createElement('div')
+            item.innerText = query
+            searchHistoryList.appendChild(item)
+        });
+    };
+
+    searchHistory.slice(-3).forEach(createHistiryItem);
+
+    window.addEventListener('storage', (event: StorageEvent) => {
+        if (event.key === 'searchHistory') {
+            searchHistory = getSearchHistory();
+            updateSearchHistory();
+        }
+    });
+
+    searchInput.addEventListener('input', debounce(async (): Promise<void> => {
         suggestionsList.innerHTML = ''
         if (searchInput.value === "") return
-        const query = encodeURIComponent(searchInput.value) 
-        const { meals } = await getData(
-            `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
-        ) as { meals: Record<string, any>[] | null }
-        if (!meals) return
+        const query = encodeURIComponent(searchInput.value)
+        try {
+            const { meals } = await getData(
+                `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+            ) as { meals: Record<string, any>[] | null }
+            if (!meals) return
 
-        const inputText = searchInput.value.toLowerCase()
-        const results: Record<string, any>[] = searchHistory
-            .filter((q: string) => q.toLowerCase().includes(inputText))
-            .map((q: string) => ({
-                name: q,
-                fromHistory: true,
-                data: meals.find(m => m.strMeal === q)
-            }))
-            .slice(0, 5)
-        results.push(
-            ...meals
-                .filter(m => !results.find((item) => item.name === m.strMeal))
-                .slice(0, 10 - results.length)
-                .map((m: Record<string, unknown>) => ({
-                    name: m.strMeal,
-                    fromHistory: false,
-                    data: m
+            const inputText = searchInput.value.toLowerCase()
+            const results: Record<string, any>[] = searchHistory
+                .filter((q: string) => q.toLowerCase().includes(inputText))
+                .map((q: string) => ({
+                    name: q,
+                    fromHistory: true,
+                    data: meals.find(m => m.strMeal === q)
                 }))
-        )
+                .slice(0, 5)
+            results.push(
+                ...meals
+                    .filter(m => !results.find((item) => item.name === m.strMeal))
+                    .slice(0, 10 - results.length)
+                    .map((m: Record<string, unknown>) => ({
+                        name: m.strMeal,
+                        fromHistory: false,
+                        data: m
+                    }))
+            )
 
-        results.forEach((item) => {
-            const elem = document.createElement('div')
-            elem.innerText = item.name
-            suggestionsList.appendChild(elem)
-            elem.addEventListener('click', async () => {
-                if (!item.fromHistory) {
-                    searchHistory.push(item.name) 
-                    localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
-                }
-                
-                fullInfo.innerHTML = `
-                    <div><img class="img" src="${item.data.strMealThumb}" alt></div>
-                    <div>${item.name}</div>
-                `
-                suggestionsList.innerHTML = ''
-                searchHistoryList.innerHTML = ''
-                searchHistory.slice(-3).forEach((query: string) => {
-                    const item = document.createElement('div')
-                    item.innerText = query
-                    searchHistoryList.appendChild(item)
+            results.forEach((item) => {
+                const elem = document.createElement('div')
+                elem.innerText = item.name
+                suggestionsList.appendChild(elem)
+                elem.addEventListener('click', async () => {
+                    if (!item.fromHistory) {
+                        searchHistory.push(item.name) 
+                        localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
+                        updateSearchHistory();
+                    }
+                    
+                    fullInfo.innerHTML = `
+                        <div><img class="img" src="${item.data.strMealThumb}" alt></div>
+                        <div>${item.name}</div>
+                    `
+                    suggestionsList.innerHTML = ''
                 })
             })
-        })
+        }
+        catch (e) {
+            alert('Произошла ошибка при взаимодействии с сервером');
+        }
     }, 500))
 })
